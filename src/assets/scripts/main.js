8dd142dd8fca42a1fb829cb0dccc725eab91dbbe
Tony M <tony@fbpllc.com>
2020-08-20 16:05:58 -0700

    add page animation

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,2 +1,37 @@
-const string = 'main';
-console.log(string);
+function fadeInPage() {
+  if (!window.AnimationEvent) {
+    return;
+  }
+  const fader = document.getElementById('fader');
+  fader.classList.add('fade-out');
+}
+
+fadeInPage();
+document.addEventListener('DOMContentLoaded', () => {
+  if (!window.AnimationEvent) {
+    return;
+  }
+  const anchors = document.getElementsByTagName('a');
+  for (let i = 0; i < anchors.length; i++) {
+    anchors[i].addEventListener('click', (e) => {
+      const fader = document.getElementById('fader');
+      const anchor = e.currentTarget;
+
+      const listener = function () {
+        window.location = anchor.href;
+      };
+
+      fader.addEventListener('animationend', listener);
+
+      e.preventDefault();
+      fader.classList.add('fade-in');
+    });
+  }
+});
+window.addEventListener('pageshow', (e) => {
+  if (!e.persisted) {
+    return;
+  }
+  const fader = document.getElementById('fader');
+  fader.classList.remove('fade-in');
+});
