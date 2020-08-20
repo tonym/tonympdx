a52fe897bc99ee8d05fc0dc82a8c3221b6d017d4
Tony M <tony@fbpllc.com>
2020-08-20 10:00:50 -0700

    config and file structure

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('CNAME');
+  eleventyConfig.addPassthroughCopy('src/images');
+  eleventyConfig.addPassthroughCopy('src/scripts');
+  eleventyConfig.addPassthroughCopy('src/styles');
   return {
     dir: {
       input: 'src'
