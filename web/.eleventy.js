848825ddc5d7058f76c5411acfb38483bc82e479
tonym <tony@fbpllc.com>
2022-08-04 15:56:46 -0700

    connect to Sanity

--- web/.eleventy.js
+++ web/.eleventy.js
@@ -1,12 +1,57 @@
+const { DateTime } = require("luxon");
+const util = require('util')
+const CleanCSS = require("clean-css");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('CNAME');
   eleventyConfig.addPassthroughCopy('src/assets/images');
 
+  // https://www.11ty.io/docs/quicktips/inline-css/
+  eleventyConfig.addFilter("cssmin", function(code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+
+  eleventyConfig.addFilter("debug", function(value) {
+    return util.inspect(value, {compact: false})
+   });
+
+   eleventyConfig.addFilter("readableDate", dateObj => {
+    return new Date(dateObj).toDateString()
+  });
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+  });
+
+  let markdownIt = require("markdown-it");
+  let markdownItAnchor = require("markdown-it-anchor");
+  let options = {
+    html: true,
+    breaks: true,
+    linkify: true
+  };
+  let opts = {
+    permalink: true,
+    permalinkClass: "direct-link",
+    permalinkSymbol: "#"
+  };
+
+  eleventyConfig.setLibrary("md", markdownIt(options)
+    .use(markdownItAnchor, opts)
+  );
+
+  eleventyConfig.addFilter("markdownify", function(value) {
+    const md = new markdownIt(options)
+    return md.render(value)
+  })
+
   return {
     dir: {
-      input: 'src',
+      data: '_data',
       htmlTemplateEngine: 'njk',
       includes: '_includes',
+      input: 'src',
       layouts: '_layouts',
       markdownTemplateEngine: 'njk',
       templateFormats: ['md', 'njk']
---
+++ web/client-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  sanity: {
+    projectId: '5pnyimif',
+    dataset: 'production'
+  }
+}
