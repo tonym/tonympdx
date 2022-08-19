3ec35ff8b6277534f65d5b974b649968eceab12d
tonym <tony@fbpllc.com>
2022-08-19 13:03:48 -0700

    blog format post

--- web/.eleventy.js
+++ web/.eleventy.js
@@ -6,12 +6,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/assets/static');
 
   eleventyConfig.addFilter("readableDate", dateObj => {
-    return new Date(dateObj).toDateString()
-  });
-
-  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+    return DateTime.fromISO(dateObj).toFormat('dd LLL yyyy');
   });
 
   let markdownIt = require("markdown-it");
